b054e8d3d8556a43f9586744da9941286d8a4cd7
AlbertoGarrote <sugaryspider822@gmail.com>
2025-10-28 19:26:20 +0100

    PathFiniding A* Implementado

--- Assets/_OurAssets/Scripts/Game/Entity/Unit/Unit.cs
+++ Assets/_OurAssets/Scripts/Game/Entity/Unit/Unit.cs
@@ -36,7 +36,19 @@ namespace CursedOnion.Game.Entity
             turnSystem = levelManager.GetTurnSystem();
             
             levelGrid.GetTileAtWorldPosition(transform.position).SetContainedEntity(this);
-            
+
+            // Controllers
+            if (UnitController == null)
+            {
+                UnitController = GetComponent<UnitController>();
+                if (UnitController == null)
+                {
+                    UnitController = IsEnemy
+                        ? gameObject.AddComponent<AIUnitController>()
+                        : gameObject.AddComponent<PlayerUnitController>();
+                }
+            }
+
             Debug.Log("El set de stats es temporal");
             Stats.SetStats(Data);
         }
@@ -66,20 +78,10 @@ namespace CursedOnion.Game.Entity
             return target != null;
         }
 
-        /*public void Move(Vector3 newPosition) 
-        {
-            uiScript.SetButtonsFalse();
-            Debug.Log(characterName + id + " Moving to " + newPosition);
-            canMove = false;
-            hasMoved = true;
-            if (moveCoroutine != null)
-                StopCoroutine(moveCoroutine);
-
-            moveCoroutine = StartCoroutine(MoveAlongPath(GetPath(transform.position, newPosition)));
-        }*/
 
         protected override void DoMove(Vector3 newPosition, bool undo)
         {
+            
             if (undo)
             {
                 transform.position = newPosition;
@@ -87,6 +89,24 @@ namespace CursedOnion.Game.Entity
             else
             {
                 Debug.Log($"{gameObject.name}: Me muevo a {newPosition}");
+                if (moveCoroutine != null)
+                    StopCoroutine(moveCoroutine);
+
+                if (!levelGrid.TryWorldToGridPosition(transform.position, out Vector3 startGrid))
+                {
+                    Debug.LogError($"TryWorldToGridPosition falló para start world position: {transform.position}");
+                    return;
+                }
+
+                var path = UnitController.PathFinder.FindPath(startGrid, newPosition, levelGrid);
+
+                if (path == null || path.Count == 0)
+                {
+                    Debug.LogWarning("No se encontró camino (FindPath devolvió null/empty).");
+                    return;
+                }
+
+                moveCoroutine = StartCoroutine(MoveAlongPath(path));
             }
         }
 
@@ -95,110 +115,14 @@ namespace CursedOnion.Game.Entity
             return true;
         }
 
-        
-        #region PathFinding
-        // Pathfinding method (Bresenham's 3D line algorithm)
-        public static List<Vector3> GetPath(Vector3 start, Vector3 end) // From repository with MIT License
-        {
-            List<Vector3> path = new List<Vector3>();
-
-            float x1 = start.x, y1 = start.y, z1 = start.z;
-            float x2 = end.x, y2 = end.y, z2 = end.z;
-            
-            float dx = Mathf.Abs(x2 - x1), dy = Mathf.Abs(y2 - y1), dz = Mathf.Abs(z2 - z1);
-            float xs = x2 > x1 ? 1 : -1;
-            float ys = y2 > y1 ? 1 : -1;
-            float zs = z2 > z1 ? 1 : -1;
-            
-            float p1, p2;
-            
-            float x = x1, y = y1, z = z1;
-
-            // X is the driving axis
-            if (dx >= dy && dx >= dz)
-            {
-                p1 = 2 * dy - dx;
-                p2 = 2 * dz - dx;
-                while (x != x2)
-                {
-                    path.Add(new Vector3(x, y, z));
-                    x += xs;
-                    if (p1 >= 0)
-                    {
-                        y += ys;
-                        p1 -= 2 * dx;
-                    }
-                    if (p2 >= 0)
-                    {
-                        z += zs;
-                        p2 -= 2 * dx;
-                    }
-                    p1 += 2 * dy;
-                    p2 += 2 * dz;
-                }
-            }
-            // Y is the driving axis
-            else if (dy >= dx && dy >= dz)
-            {
-                p1 = 2 * dx - dy;
-                p2 = 2 * dz - dy;
-                while (y != y2)
-                {
-                    path.Add(new Vector3(x, y, z));
-                    y += ys;
-                    if (p1 >= 0)
-                    {
-                        x += xs;
-                        p1 -= 2 * dy;
-                    }
-                    if (p2 >= 0)
-                    {
-                        z += zs;
-                        p2 -= 2 * dy;
-                    }
-                    p1 += 2 * dx;
-                    p2 += 2 * dz;
-                }
-            }
-            // Z is the driving axis
-            else
-            {
-                p1 = 2 * dy - dz;
-                p2 = 2 * dx - dz;
-                while (z != z2)
-                {
-                    path.Add(new Vector3(x, y, z));
-                    z += zs;
-                    if (p1 >= 0)
-                    {
-                        y += ys;
-                        p1 -= 2 * dz;
-                    }
-                    if (p2 >= 0)
-                    {
-                        x += xs;
-                        p2 -= 2 * dz;
-                    }
-                    p1 += 2 * dy;
-                    p2 += 2 * dx;
-                }
-            }
-
-            path.Add(new Vector3(x2, y2, z2)); // Añade el destino
-            return path;
-        }
         private IEnumerator MoveAlongPath(List<Vector3> path)
         {
             foreach (var pos in path)
             {
                 transform.position = new Vector3(pos.x, pos.y, pos.z);
-                yield return new WaitForSeconds(0.5f);
+                yield return new WaitForSeconds(0.25f);
             }
-
-            //UpdateCharacterUI();
         }
-        #endregion
-
 
     }
 }
--- Assets/_OurAssets/Scripts/Game/Entity/Unit/UnitController.cs
+++ Assets/_OurAssets/Scripts/Game/Entity/Unit/UnitController.cs
@@ -1,12 +1,23 @@
+using CursedOnion.Game.Systems.Grid;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace CursedOnion.Game.Entity
 {
     public abstract class UnitController : MonoBehaviour
     {
+        public AStarPathFinder PathFinder;
+
+        protected virtual void Awake()
+        {
+            if (PathFinder == null)
+                PathFinder = new AStarPathFinder();
+        }
         public abstract void ProcessTurn();
     }
-    
+
     public class PlayerUnitController : UnitController
     {
         public override void ProcessTurn()
@@ -19,6 +30,136 @@ namespace CursedOnion.Game.Entity
         public override void ProcessTurn()
         {
             throw new System.NotImplementedException();
+        }
+    }
+
+    public class AStarPathFinder
+    {
+        public List<Vector3> FindPath(Vector3 startGrid, Vector3 targetGrid, Grid3d levelGrid)
+        {
+       
+            List<Node> openList = new List<Node>();
+            HashSet<Vector3> closedList = new HashSet<Vector3>();
+
+            Node startNode = new Node(startGrid, null, 0, Heuristic(startGrid, targetGrid));
+            openList.Add(startNode);
+
+            while (openList.Count > 0)
+            {
+                Node currentNode = openList.OrderBy(n => n.f).First();
+                openList.Remove(currentNode);
+                closedList.Add(currentNode.gridPos);
+
+                if (currentNode.gridPos == targetGrid)
+                {
+                    return ReconstructPath(currentNode, levelGrid);
+                }
+
+                // Explorar vecinos
+                foreach (Vector3 neighbourPos in GetNeighbours(currentNode.gridPos, levelGrid))
+                {
+                    if (closedList.Contains(neighbourPos))
+                        continue;
+
+                    Tile3d tile = levelGrid.GetTileAtGridPosition(neighbourPos);
+                    if (tile == null || tile.GetContainedEntity() != null)
+                        continue; // Tile obstaculo
+
+                    float tentativeG = currentNode.g + Vector3.Distance(currentNode.gridPos, neighbourPos);
+
+                    Node existingNode = openList.FirstOrDefault(n => n.gridPos == neighbourPos);
+                    if (existingNode != null && tentativeG >= existingNode.g)
+                        continue;
+
+                    float h = Heuristic(neighbourPos, targetGrid);
+                    Node neighbourNode = new Node(neighbourPos, currentNode, tentativeG, h);
+
+                    if (existingNode != null)
+                        openList.Remove(existingNode);
+
+                    openList.Add(neighbourNode);
+                }
+            }
+
+            // Si salimos del bucle, no hay camino
+            Debug.Log("No se encontró un camino.");
+            return null;
+        }
+
+        private List<Vector3> GetNeighbours(Vector3 gridPos, Grid3d levelGrid)
+        {
+            List<Vector3> neighbours = new List<Vector3>();
+            List<Vector3> directions = new List<Vector3>
+        {
+            new Vector3( 1, 0,  0),
+            new Vector3(-1, 0,  0),
+            new Vector3( 0, 0,  1),
+            new Vector3( 0, 0, -1),
+
+            // Diagonales
+            new Vector3( 1, 0,  1),
+            new Vector3( 1, 0, -1),
+            new Vector3(-1, 0,  1),
+            new Vector3(-1, 0, -1)
+        };
+
+            foreach (var dir in directions)
+            {
+                Vector3 neighbour = gridPos + dir;
+
+                if (levelGrid.GetTileAtGridPosition(neighbour) != null)
+                    neighbours.Add(neighbour);
+            }
+
+            return neighbours;
+        }
+
+
+        private float Heuristic(Vector3 a, Vector3 b)
+        {
+            return Vector3.Distance(a, b);
+        }
+
+        private List<Vector3> ReconstructPath(Node endNode, Grid3d levelGrid)
+        {
+            List<Vector3> path = new List<Vector3>();
+            Node current = endNode;
+
+            while (current != null)
+            {
+                path.Add(current.gridPos);
+                current = current.parent;
+            }
+
+            path.Reverse();
+
+            // Convertir a coordenadas del mundo
+            for (int i = 0; i < path.Count; i++)
+            {
+                if (levelGrid.TryGridToWorldPosition(path[i], out Vector3 worldPos))
+                {
+                    worldPos += new Vector3(0.5f, 0f, 0.5f);
+                    path[i] = worldPos;
+                }
+            }
+
+            return path;
+        }
+
+        private class Node
+        {
+            public Vector3 gridPos;
+            public Node parent;
+            public float g, h, f;
+
+            public Node(Vector3 pos, Node parent, float g, float h)
+            {
+                this.gridPos = pos;
+                this.parent = parent;
+                this.g = g;
+                this.h = h;
+                this.f = g + h;
+            }
         }
     }
 }
